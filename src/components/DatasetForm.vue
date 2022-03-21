dc86dd9b7dc47933b66c0cf2395b520cc3b5f710
THANATATH <thanatuth.euro@gmail.com>
2022-03-21 23:25:19 +0700

    fix error when add dataset of blank value

--- src/components/DatasetForm.vue
+++ src/components/DatasetForm.vue
@@ -12,8 +12,7 @@
           <v-col cols="4">
             <v-img
               lazy-src="../assets/banner.jpg"
-              max-height="150"
-              max-width="250"
+              max-height="100%"
               :src="url"
             ></v-img>
             <v-file-input
@@ -21,7 +20,7 @@
               accept="image/png, image/jpeg, image/gif"
               placeholder="Select an image"
               prepend-icon="mdi-file-image"
-              label="Click for select image"
+              label="Click here for select image"
               show-size
               @change="onFileChange"
             ></v-file-input>
@@ -29,17 +28,24 @@
 
           <v-col cols="8">
             <!-- Question -->
+
             <v-text-field
               id="question"
               v-model="question"
               :rules="[rules.required]"
               label="Question"
               required
+              placeholder="Question for dataset"
               outlined
               dense
             ></v-text-field>
             <!-- textchip -->
+
+            <v-alert v-show="answer.length>=5" dismissible color="red" text type="info"
+              >Max set of answer is 5 !!!</v-alert
+            >
             <v-text-field
+            placeholder="Answer set for dataset"
               label="Answer (max 5)"
               v-model="textchip"
               v-on:keyup.enter="addChips()"
@@ -108,11 +114,10 @@ export default {
       textchip: '',
       answer: [],
       url: null,
-      datasetAPI:process.env.VUE_APP_DATASET_URL,
+      datasetAPI: process.env.VUE_APP_DATASET_URL,
       rules: {
         required: (value) => !!value || 'Required.',
       },
-      
     };
   },
   methods: {
@@ -130,11 +135,11 @@ export default {
       }
       //edit key
       else if (this.isEdit) {
-      let DatasetFormData = new FormData();
-      DatasetFormData.append('id', this.id);
-      DatasetFormData.append('dataset_image_upload', this.files);
-      DatasetFormData.append('new_dataset_question', this.question);
-      DatasetFormData.append('new_dataset_reply', this.answer);
+        let DatasetFormData = new FormData();
+        DatasetFormData.append('id', this.id);
+        DatasetFormData.append('dataset_image_upload', this.files);
+        DatasetFormData.append('new_dataset_question', this.question);
+        DatasetFormData.append('new_dataset_reply', this.answer);
         this.$store.dispatch('editDataset', DatasetFormData).then(() => {
           this.cancel();
         });
@@ -145,7 +150,6 @@ export default {
     },
     addChips() {
       if (this.textchip.length && this.answer.length < 5) {
-         
         this.answer.push(this.textchip);
         this.textchip = '';
       }
@@ -159,9 +163,14 @@ export default {
   },
   mounted() {
     this.id = this.dataset_id;
-    this.url = this.datasetAPI+this.dataset_img;
+    this.url = this.datasetAPI + this.dataset_img;
     this.question = this.dataset_question;
-    this.answer = this.dataset_reply;
+    if (this.dataset_reply) {
+      this.answer = this.dataset_reply;
+    } else {
+      this.answer = [];
+      this.question = '';
+    }
   },
 };
 </script>
